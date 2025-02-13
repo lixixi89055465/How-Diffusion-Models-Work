e3601ce9df9843943dd3692b00adeecf67737b75
lixixi89055465 <nanji1850094299@outlook.com>
2025-02-13 23:35:33 +0800

    init

---
+++ stu/diffusion_util.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from torchvision.utils import save_image, make_grid
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, PillowWriter
+import os
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset
+from PIL import Image
+
+
+class ResidualConvBlock(nn.Module):
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 is_res: bool = False,
+                 ) -> None:
+        super().__init__()
+        self.same_channels = in_channels == out_channels
+        self.is_res = is_res
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1),
+            nn.BatchNorm2d(out_channels),
+            nn.GELU(),  # GELU activation function
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1),
+            nn.BatchNorm2d(out_channels),
+            nn.GELU()
+        )
